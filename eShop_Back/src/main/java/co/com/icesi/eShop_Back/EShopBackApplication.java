8e0e2090c86296ca924e1f81abb6200aca03594e
Carlos Arturo Diaz <70271822+ArturoDiaz02@users.noreply.github.com>
2023-05-23 17:05:21 -0500

    init

---
+++ eShop_Back/src/main/java/co/com/icesi/eShop_Back/EShopBackApplication.java
@@ -0,0 +1,13 @@
+package co.com.icesi.eShop_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShopBackApplication.class, args);
+	}
+
+}
