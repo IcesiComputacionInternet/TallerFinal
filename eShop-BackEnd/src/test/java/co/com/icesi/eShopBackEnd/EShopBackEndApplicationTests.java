b399e3e66d165f6b1d5ef624f1650bfd6e37b46f
DaniBonica001 <1107834340@u.icesi.edu.co>
2023-05-28 12:04:24 -0500

    spring initializer backEnd

---
+++ eShop-BackEnd/src/test/java/co/com/icesi/eShopBackEnd/EShopBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.icesi.eShopBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
