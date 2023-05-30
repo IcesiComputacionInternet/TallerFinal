4014d01e5df4a46a4e166b32ae3aee0f10a3da23
Mateo <mateora73@gmail.com>
2023-05-29 20:11:37 -0500

    Register page

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,43 +1,76 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import myImage from '../resources/logo.png'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Divider  from '@mui/material/Divider'
+import { createTheme } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import Router from 'next/router'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#022b3a"
+    },
+    secondary: {
+      main: "#1F7A8C"
+    }
+  }
+})
 
 
 const Home: NextPage = () => {
-  
+  //const navigation : NavigateFunction = useNavigate(); 
+  const handleRegister = () => {
+    Router.push("/register");
+  }
+  const handleLogin = () => {
+    Router.push("/home");
+  }
+
   return (
     <div className={styles.flexContainer}>
+      <ThemeProvider theme={theme}>
       <div className={styles.section1}>
         <div>
           <h1>Bienvenido</h1>
         </div>
         <div className={styles.form}>
-            <h3>Nombre de usuario</h3>
-            <TextField id="outlined-basic" 
-            label="Usuario" 
-            variant="outlined" 
-            sx={{maxWidth: 250}} 
-            size='small' 
-            margin='normal'/>
-
+          <div>
+            <h4 style={{margin:0}}>Username</h4>
+              <TextField id="outlined-basic" 
+              label="Username" 
+              variant="outlined" 
+              sx={{maxWidth: 250}} 
+              size='small' 
+              />
+          </div>
+          <div>
+            <h4 style={{margin:0}}>Password</h4>
             <TextField 
             id="outlined-basic" 
-            label="Contraseña" 
+            label="Password" 
             variant="outlined" 
             sx={{maxWidth: 250}} 
-            size='small' 
-            margin='normal'
+            size='small'             
             type='password'/>
+          </div>
+            
+            
         </div>
         <Divider sx={{marginTop: 2, marginBottom: 2}}/>
-        <div className={styles.buttons}>
-          <Button variant="contained" sx={{maxWidth: 170}} >Iniciar Sesión</Button>
-          <Button variant="outlined" sx={{maxWidth: 170, marginLeft:5}}>Registrarse</Button>
+        <div className={styles.buttons}>  
+          <Button 
+            variant="contained" 
+            sx={{maxWidth: 170, minWidth: 100}} 
+            color="primary"
+            onClick={handleLogin}>Log In</Button>
+          <Button 
+            variant="outlined" 
+            sx={{maxWidth: 170, marginLeft:5, minWidth: 100}} 
+            color="secondary"
+            onClick={handleRegister}>Register</Button>
         </div>
       </div>
       <div className={styles.section2}>
@@ -45,10 +78,12 @@ const Home: NextPage = () => {
           <img src={myImage.src} alt="Logo" width={150} height={150}/>
         </div>
         <div>
-          <h1> E-Shop de televisores.com</h1>
+          <h1>TV-EShop.com</h1>
         </div>
       </div>
+      </ThemeProvider>
     </div>
+    
   )
 }
 
---
+++ front-end/pages/register.tsx
@@ -0,0 +1,75 @@
+import { NextPage } from "next";
+import styles from '../styles/Home.module.css'
+import { Button, TextField } from "@mui/material";
+import { createTheme } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import Router from 'next/router'
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#1F7A8C"
+      },
+      secondary: {
+        main: "#022b3a"
+        
+      }
+    }
+  })
+
+const Register:NextPage = () => {
+    const handleCancel = () => {
+        Router.push("/");
+    }
+
+    return (
+        <div className={styles.flexContainer}>
+            <div className={styles.sideImage}>
+                
+            </div>
+                <div className={styles.formSection}>
+                    <h1 style={{marginLeft:45}}>Register</h1>
+                    <div style={{display:"flex",flexGrow:1,flexDirection:"row"}}> 
+                        <div className={styles.formColumn}>
+                            <div className={styles.inputNLabel}>
+                                <h4 className={styles.label}>First name</h4>
+                                <TextField id="outlined-basic" label="First Name" variant="outlined" size="small" sx={{marginBottom:2}}/>
+                            </div>
+                            <div className={styles.inputNLabel}>
+                                <h4 className={styles.label}>Last name</h4>
+                                <TextField id="outlined-basic" label="Last Name" variant="outlined" size="small" sx={{marginBottom:2}}/>
+                            </div>
+                            <div className={styles.inputNLabel}>
+                                <h4 className={styles.label}>E-Mail</h4>
+                                <TextField id="outlined-basic" label="E-Mail" variant="outlined" size="small" sx={{marginBottom:2}}/>
+                            </div>
+                        </div>
+                        <div className={styles.formColumn}>
+                            <div className={styles.inputNLabel}>
+                                <h4 className={styles.label}>Phone Number</h4>
+                                <TextField id="outlined-basic" label="Phone Number" variant="outlined" size="small" sx={{marginBottom:2}}/>
+                            </div>
+                        <div className={styles.inputNLabel}>
+                            <h4 className={styles.label}>Address</h4>
+                            <TextField id="outlined-basic" label="Address" variant="outlined" size="small" sx={{marginBottom:2}}/>
+                        </div>
+                        <div className={styles.inputNLabel}>
+                            <h4 className={styles.label}>Birthday</h4>
+                            <TextField  size="small" type="date" sx={{marginBottom:2}}></TextField>
+                        </div>
+                    </div> 
+                </div>   
+                <ThemeProvider theme={theme}>
+                    <div className={styles.registerButtons}>
+                        <Button variant="contained" sx={{marginRight:2}}>Register</Button>
+                        <Button variant="contained" sx={{marginLeft:2}} color="secondary" onClick={handleCancel}>Cancel</Button>
+                    </div>    
+                </ThemeProvider>
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default Register
