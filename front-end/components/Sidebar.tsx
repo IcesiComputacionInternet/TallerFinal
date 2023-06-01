e7ea265600b5c0c36e5384c35d666c6e33af92fa
Mateo <mateora73@gmail.com>
2023-06-01 08:38:20 -0500

    Side bar complete

--- front-end/components/Sidebar.tsx
+++ front-end/components/Sidebar.tsx
@@ -1,42 +1,82 @@
 import { NextPage } from "next";
-import myLogo from "../resources/logo.png"
+import myLogo from "../resources/logo.png";
 import SideButton from "./SideButton";
+import InventoryIcon from '@mui/icons-material/Inventory';
+import { Divider } from "@mui/material";
+import styles from "../styles/Sidebar.module.css";
+import HomeIcon from '@mui/icons-material/Home';
+import CategoryIcon from '@mui/icons-material/Category';
+import PeopleIcon from '@mui/icons-material/People';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Router from "next/router";
 
 const items  = [
     {
         name:"Home",
-        path:"/home"
+        path:"/home",
+        icon:<HomeIcon sx={{color:"#e6e6e9"}}/>,
+        onClick:() => {
+            Router.push("/home");
+        }
     },
     {
         name:"Products",
-        path:"/products"
+        path:"/products",
+        icon:<InventoryIcon sx={{color:"#e6e6e9"}}/>,
+        onClick:() => {
+            Router.push("/products");
+        }
     },
     {
         name:"Users",
-        path:"/users"
+        path:"/users",
+        icon:<PeopleIcon sx={{color:"#e6e6e9"}}/>,
+        onClick:() => {
+            Router.push("/users");
+        }
     },
     {
         name:"Orders",
-        path:"/orders"
+        path:"/orders",
+        icon:<ListAltIcon sx={{color:"#e6e6e9"}}/>,
+        onClick:() => {
+            Router.push("/orders");
+        }
     },
     {
         name:"Categories",
-        path:"/categories"
+        path:"/categories",
+        icon:<CategoryIcon sx={{color:"#e6e6e9"}}/>,
+        onClick:() => {
+            Router.push("/categories");
+        }
     }];
 
 
 const Sidebar : NextPage = () => {
-    return (
-        <div style={{display:"flex",height:"calc(100%)",minWidth:200,backgroundColor:"#595959",flexDirection:"column"}}>
-            <div className="logo" style={{display:"flex", alignItems:"center",justifyContent:"center",backgroundColor:"white"}}>
-                <img src={myLogo.src} alt="Logo" style={{width:90,height:90}}/>
-                <h2>E-Shop</h2>
-            </div>
-            <div className="buttons">
-                <SideButton name="Home" path="/" icon={null}></SideButton>
+    const handleLogout = () => {
+        localStorage.clear();
+        Router.push("/");
+    }
 
+    return (
+        <div className={styles.bar}>
+            <div className={styles.logoContainer} >
+                <img src={myLogo.src} alt="Logo" style={{width:90,height:90}}/>
+                <h2 className={styles.logoTitle}>E-Shop</h2>
             </div>
-            <h1>Sidebar</h1>
+            <div className={styles.buttonsContainer}>
+                <Divider sx={{backgroundColor:"#e6e6e9"}}/>
+                {items.map((item,index) => {
+                    return (
+                        <SideButton key={index} name={item.name} path={item.path} icon={item.icon} handleClick={item.onClick}/>
+                    )
+                })}
+            </div>
+            <div className={styles.logoutContainer}>
+                    <SideButton name="Logout" path="/" icon={<LogoutIcon sx={{color:"#e6e6e9"}}/>} handleClick={handleLogout}/>
+            </div> 
         </div>
     );
 }
