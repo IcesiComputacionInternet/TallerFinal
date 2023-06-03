e087dce5c4f8328aeed61e0a8df5bdd318238dc1
Rockthor <jan.carvajal1@u.icesi.edu.co>
2023-06-02 21:41:51 -0500

    Base structure for backend added

---
+++ automoviles/src/test/java/co/edu/icesi/automoviles/AutomovilesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.icesi.automoviles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomovilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
