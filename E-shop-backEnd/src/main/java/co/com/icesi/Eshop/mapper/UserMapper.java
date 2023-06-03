a5ff7e1c53bc6ec639a467f9f47dffaf3d0aea57
Johan Stiven Ricardo <johan.ricardo1104@gmail.com>
2023-06-03 09:59:16 -0500

    fix: @Mapping ofr userMapper

--- E-shop-backEnd/src/main/java/co/com/icesi/Eshop/mapper/UserMapper.java
+++ E-shop-backEnd/src/main/java/co/com/icesi/Eshop/mapper/UserMapper.java
@@ -4,16 +4,21 @@ import co.com.icesi.Eshop.dto.request.UserDTO;
 import co.com.icesi.Eshop.dto.response.UserResponseDTO;
 import co.com.icesi.Eshop.model.UserPrincipal;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "role", ignore = true)
     UserPrincipal toUser(UserDTO userDTO);
 
+    @Mapping(target = "role", ignore = true)
     UserPrincipal toUser(UserResponseDTO userResponseDTO);
 
+    @Mapping(target = "role", ignore = true)
     UserDTO toUserDTO(UserPrincipal userPrincipal);
 
+    @Mapping(target = "role", ignore = true)
     UserDTO toUserDTO(UserResponseDTO userResponseDTO);
 
     UserResponseDTO toUserResponseDTO(UserPrincipal userPrincipal);
