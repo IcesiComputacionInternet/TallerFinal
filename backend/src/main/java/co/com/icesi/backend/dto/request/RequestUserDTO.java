962d5d111062f989f56c116fc869f37abe75fd6c
Keren <kerenlopezcordoba@outlook.es>
2023-06-04 15:37:39 -0500

    Update RequestUserDTO

--- backend/src/main/java/co/com/icesi/backend/dto/request/RequestUserDTO.java
+++ backend/src/main/java/co/com/icesi/backend/dto/request/RequestUserDTO.java
@@ -12,13 +12,10 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RequestUserDTO {
-    @NotBlank(message = "The first name of a user can't be blank")
     private String firstName;
 
-    @NotBlank(message = "The lastname of a user can't be blank")
     private String lastName;
 
-    @NotNull(message = "The email of a user can't be null")
     @Email(message = "Invalid email address")
     private String email;
 
@@ -26,14 +23,11 @@ public class RequestUserDTO {
     @NotNull(message = "The password of a user can't be null")
     private String password;
 
-    @NotNull(message = "The phone number of a user can't be null")
     //AGREGAR PHONE CONSTRAINT!!!
     private String phoneNumber;
 
-    @NotBlank(message = "The address of a user can't be blank")
     private String address;
 
-    @NotBlank(message = "The birthday of a user can't be blank")
     private String birthday;
 
     @NotNull(message = "The role of a user can't be null")
