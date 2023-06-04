1595e805390f2bf8c8ebe1d2db6595cccd2ee3c6
Santiagokmids <sntgtrochez@gmail.com>
2023-06-04 15:16:58 -0500

    First commit

---
+++ backend/src/test/java/co/edu/icesi/Eshop/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.icesi.Eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
