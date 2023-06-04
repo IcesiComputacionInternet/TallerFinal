5cd4bda62c0bee72f9f63c7ef99ad4e060c87bde
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2023-06-04 17:01:55 -0500

    Testing the imdb api

--- front-movies-app/components/Carousel.js
+++ front-movies-app/components/Carousel.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 function Carousel() {
+
+  const [movies, setMovies] = useState([])
+  const apiKey = 'k_quwg0qhg'
+
+  useEffect( () => {
+    axios.get("https://imdb-api.com/en/API/Top250Movies/k_quwg0qhg").then(response => {
+      setMovies((response.data))
+      console.log(movies)
+    }).catch(err => console.error(err))
+  }, [])
+
   return (
-    <div id="carouselExampleIndicators" class="carousel slide">
-      <div class="carousel-indicators">
+    <div id="carouselExampleIndicators" className="carousel slide">
+      <div className="carousel-indicators">
         <button
           type="button"
           data-bs-target="#carouselExampleIndicators"
           data-bs-slide-to="0"
-          class="active"
+          className="active"
           aria-current="true"
           aria-label="Slide 1"
         ></button>
@@ -26,34 +39,34 @@ function Carousel() {
           aria-label="Slide 3"
         ></button>
       </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="/carouselEx1.jpg" class="d-block w-100" alt="1" />
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="/carouselEx1.jpg" className="d-block w-100" alt="1" />
         </div>
-        <div class="carousel-item">
-          <img src="/dc.jpg" class="d-block w-100" alt="2" />
+        <div className="carousel-item">
+          <img src="/dc.jpg" className="d-block w-100" alt="2" />
         </div>
-        <div class="carousel-item">
-          <img src="/avatar.jpg" class="d-block w-100" alt="3" />
+        <div className="carousel-item">
+          <img src="/avatar.jpg" className="d-block w-100" alt="3" />
         </div>
       </div>
       <button
-        class="carousel-control-prev"
+        className="carousel-control-prev"
         type="button"
         data-bs-target="#carouselExampleIndicators"
         data-bs-slide="prev"
       >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
       </button>
       <button
-        class="carousel-control-next"
+        className="carousel-control-next"
         type="button"
         data-bs-target="#carouselExampleIndicators"
         data-bs-slide="next"
       >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
       </button>
     </div>
   );
--- front-movies-app/components/Navigation.js
+++ front-movies-app/components/Navigation.js
@@ -1,34 +1,36 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function Navigation() {
+
     return (
-        <nav class="navbar navbar-expand">
-            <div class="container-fluid">
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <nav className="navbar navbar-expand">
+            <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Features</a>
                         </li>
-                        <li class="nav-item dropdown" >
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <li className="nav-item dropdown" >
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Dropdown link
                             </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
                             </ul>
                         </li>
                     </ul>
                 </div>
-                <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                 
-                <button class="btn btn-outline-success" type="submit">Search</button>
+                <button className="btn btn-outline-success" type="submit">Search</button>
                 </form>
             </div>
         </nav>
