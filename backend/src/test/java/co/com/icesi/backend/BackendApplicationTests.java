fa117773ecfe0158acfd7379000f4e857102c5ef
Luis Miguel Ossa Arias <luismiguelossaarias05@gmail.com>
2023-06-04 10:46:26 -0500

    backend folder

---
+++ backend/src/test/java/co/com/icesi/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.icesi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
