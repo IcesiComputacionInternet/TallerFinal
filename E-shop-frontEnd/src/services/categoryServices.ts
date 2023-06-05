d1b0248258245f53c99c1aea0a1eb33a2f9867be
GabrielSB19 <gasuarez2002@gmail.com>
2023-06-05 11:31:09 -0500

    Adding the crud in the categories

--- E-shop-frontEnd/src/services/categoryServices.ts
+++ E-shop-frontEnd/src/services/categoryServices.ts
@@ -3,42 +3,41 @@ import authHeader from "./authHeader";
 
 const URL_API = "http://localhost:8080/api/categories/";
 
-interface categoryProps{
-    name: string
-    description: string
+interface categoryProps {
+  name: string;
+  description: string;
 }
 
-const create = (role:categoryProps) => {
-        return axios.post(`${URL_API}create`,role,{
-            headers: authHeader()
-        })
-}
+const create = (category: categoryProps) => {
+  return axios.post(`${URL_API}create`, category, {
+    headers: authHeader(),
+  });
+};
 
-const update = (role:categoryProps) => {
-    return axios.put(`${URL_API}update`,role,{
-        headers: authHeader()
-    })
+const update = (category: categoryProps) => {
+  return axios.put(`${URL_API}update`, category, {
+    headers: authHeader(),
+  });
+};
 
-}
+const deleteCategory = (categoryName: string) => {
+  return axios.delete(`${URL_API}delete`, {
+    data: categoryName,
+    headers: authHeader(),
+  });
+};
 
-const deleteRole = (categoryName:string) =>{
-    return axios.delete(`${URL_API}delete`,{
-        data: categoryName, 
-        headers: authHeader()
-    })
-}
-
-const getRoles = () => {
-    return axios.get(`${URL_API}/all`,{
-        headers: authHeader()
-    })
-}
+const getCategories = () => {
+  return axios.get(`${URL_API}all`, {
+    headers: authHeader(),
+  });
+};
 
 const roleServices = {
-    create, 
-    update,
-    deleteRole,
-    getRoles
-}
+  create,
+  update,
+  deleteCategory,
+  getCategories,
+};
 
-export default roleServices
+export default roleServices;
