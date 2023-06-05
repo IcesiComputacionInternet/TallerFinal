2073f3479d5ac7a46fbe5022f543c1f58b4e2c34
Martin-Cifuentes <marcisego@gmail.com>
2023-06-05 09:45:50 -0500

    dropdown working

--- front-movies-app/components/Navigation.js
+++ front-movies-app/components/Navigation.js
@@ -5,35 +5,41 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Navigation() {
 
     return (
-        <nav className="navbar navbar-expand">
-            <div className="container-fluid">
-                <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="#">Features</a>
-                        </li>
-                        <li className="nav-item dropdown" >
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+        <div>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"></link>
+            <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+        
+        
+            <nav className="navbar navbar-expand">
+                <div className="container-fluid">
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                            <a className="nav-link" href="#">Features</a>
+                            </li>
+                            <li className="nav-item dropdown" >
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown link
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Category 1</a></li>
+                                    <li><a className="dropdown-item" href="#">Category 2</a></li>
+                                    <li><a className="dropdown-item" href="#">Category 3</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <form className="d-flex" role="search">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    
+                    <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
                 </div>
-                <form className="d-flex" role="search">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                
-                <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
+            </nav>
+        </div>
       )
 }
 
