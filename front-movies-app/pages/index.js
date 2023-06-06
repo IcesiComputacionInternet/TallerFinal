1527fecc35edac6c61c46c33c002e3506b8c1baf
Martin-Cifuentes <marcisego@gmail.com>
2023-06-06 14:32:24 -0500

    card?

--- front-movies-app/pages/index.js
+++ front-movies-app/pages/index.js
@@ -1,10 +1,28 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navigation from '../components/Navigation'
 import Carousel from '../components/Carousel'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from '../components/Card';
+import axios from 'axios';
 
 export default function Home() {
+
+  const url = "http://localhost:8080/movies/"
+
+  console.log(getToken())
+
+  async function getToken() {
+    
+    return axios.post(url + "all", {
+      headers: {
+        "username": "noname@email.com",
+        "password": "password"
+      }
+    })
+
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,8 +33,8 @@ export default function Home() {
       
       <Navigation/>
       <Carousel/>
+      <Card/>
       
-
       
     </div>
       
