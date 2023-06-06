d214305e9fb846b04393c441e50001e4361fa230
GabrielSB19 <gasuarez2002@gmail.com>
2023-06-06 13:12:47 -0500

    Starting the update orders

--- E-shop-frontEnd/src/views/orders/crudOrders.tsx
+++ E-shop-frontEnd/src/views/orders/crudOrders.tsx
@@ -1,7 +1,32 @@
+import React from "react";
+import TableOrder from "./tableOrders";
+import Header from "../utils/header";
+import Footer from "../utils/footer";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
+import ModalAlert from "../utils/modalAlert";
+import OrderServices from "../../services/orderServices";
+
 const CrudOrders = () => {
+  const [orders, setOrders] = useState<string[]>([]);
+  const [dataToEdit, setDataToEdit] = useState<any>(null);
+
+  useEffect(() => {
+    try {
+      OrderServices.getOrders().then((res) => {
+        setOrders(res.data);
+        console.log(res.data);
+      });
+    } catch (error) {
+      ModalAlert.ModalAlertError("Error", "Error al cargar las ordenes");
+    }
+  }, []);
   return (
     <>
-      <div>Hola</div>
+      <Header />
+      <TableOrder data={orders} setDataToEdit={setDataToEdit} />
+      <Footer />
     </>
   );
 };
