da7b56c3568d240d8eb8fa680d23a8a29c3b1ed4
DianaSofiaOlano <1005967885@u.icesi.edu.co>
2023-06-06 20:14:45 -0500

    Component to choose roles added in register page

---
+++ frontend/src/NotFound.tsx
@@ -0,0 +1,9 @@
+function NotFound() {
+    return (
+      <h1 style={{ textAlign: 'center', fontWeight: 'bold', color: 'red' }}>
+        Lo siento, no tienes cuenta en esta aplicación
+      </h1>
+    );
+  }
+  
+  export default NotFound;
