64f3267e6f290f5e981b06411faedf63575314d4
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2023-06-08 16:08:03 -0500

    Annotations for notBlank fields at RoleDTO added

--- e-shop-backend/src/main/java/com/example/eshopbackend/dto/RoleDTO.java
+++ e-shop-backend/src/main/java/com/example/eshopbackend/dto/RoleDTO.java
@@ -3,10 +3,16 @@ package com.example.eshopbackend.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Builder
 @Data
 public class RoleDTO {
     private String roleId;
+
+    @NotBlank(message = "Role name is required")
     private String roleName;
+
+    @NotBlank(message = "Role description is required")
     private String description;
 }
