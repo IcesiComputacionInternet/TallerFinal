61c359505e08cd33a36fb9f2f5f3538a6fdac2ee
Martin-Cifuentes <marcisego@gmail.com>
2023-06-07 21:38:14 -0500

    we gotta make a movie

--- front-movies-app/pages/Home.js
+++ front-movies-app/pages/Home.js
@@ -4,28 +4,34 @@ import Navigation from '../components/Navigation'
 import Carousel from '../components/Carousel'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from '../components/Card';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from "axios";
 
 function Home () {
 
-  const movies = getMovies();
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies()
+    console.log(movies)
+  }, [])
 
   async function getMovies() {
-    console.log("Hola")
-    return axios.get("http://localhost:8080/movies/all",
-      {
-          headers: {
-              "Access-Control-Allow-Origin": "http://localhost:8080",
-              "MediaType": "application/json",
-              "Authorization": "Bearer " + localStorage.getItem('jwt')
-          }
+    const {data} = await axios.get("http://localhost:8080/movies/all",
+    {
+        headers: {
+            "Access-Control-Allow-Origin": "http://localhost:8080",
+            "MediaType": "application/json",
+            "Authorization": "Bearer " + localStorage.getItem('jwt')
+        }
 
-      }
-    )
+    })
+    let res = {data}
+    setMovies(res)
   }
 
-  console.log(movies)
+  
 
   return (
         <div className={styles.container}>
