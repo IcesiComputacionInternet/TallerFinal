e36a127904d848d927ac615426dfb53052138ccc
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2023-06-08 20:33:11 -0500

    CategoryApi and CategoryController implemented

---
+++ e-shop-backend/src/main/java/com/example/eshopbackend/controller/CategoryController.java
@@ -0,0 +1,18 @@
+package com.example.eshopbackend.controller;
+
+import com.example.eshopbackend.api.CategoryApi;
+import com.example.eshopbackend.dto.CategoryDTO;
+import com.example.eshopbackend.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class CategoryController implements CategoryApi {
+
+    private final CategoryService categoryService;
+    @Override
+    public void createCategory(CategoryDTO categoryDTO) {
+        categoryService.save(categoryDTO);
+    }
+}
