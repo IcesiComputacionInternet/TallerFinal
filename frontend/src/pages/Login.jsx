235e0741ee6d90d2679d79e8f4c3c389cd964e40
BrayanOB2003 <brayanortizbastidas82@gmail.com>
2023-06-08 23:21:00 -0500

    Register and Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Grid } from '@mui/material';
 
-const Login = () => {
+function Login() {
+  const [contact, setContact] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleContactChange = (event) => {
+    setContact(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('Contact:', contact);
+    console.log('Password:', password);
+  };
+
   return (
-    <div>
-      <h2>Iniciar sesión</h2>
-      {/* Aquí puedes agregar el formulario de inicio de sesión */}
-    </div>
+    <form onSubmit={handleSubmit}>
+        <Grid container justifyContent="center" alignItems="center" style={{ height: '100vh' }}>
+            <Grid item>
+                <Grid container direction="column" alignItems="center" spacing={2}>
+                    <Grid item>
+                        <h1>Login</h1>
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            label="Email or Phone Number"
+                            type="text"
+                            value={contact}
+                            onChange={handleContactChange}
+                            required
+                        />
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            label="Password"
+                            type="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            required
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button type="submit" variant="contained" color="primary" fullWidth>
+                            Login
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    </form>
   );
-};
+}
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Grid, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@mui/material';
+import { Navigate } from "react-router-dom";
 
-const Register = () => {
+function Register() {
+  const [contact, setContact] = useState('');
+  const [password, setPassword] = useState('');
+  const [contactType, setContactType] = useState('email');
+  const [redirect, setRedirect] = useState(false)
+
+  const handleContactChange = (event) => {
+    setContact(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleContactTypeChange = (event) => {
+    setContactType(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('Contact Type:', contactType);
+    console.log('Contact:', contact);
+    console.log('Password:', password);
+  };
+
+  const handleLoginRedirect = () => {
+    setRedirect(true);
+  };
+
   return (
-    <div>
-      <h2>Registro</h2>
-      {/* Aquí puedes agregar el formulario de registro */}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <Grid container justifyContent="center" alignItems="center" style={{ height: '100vh' }}>
+        <Grid item>
+          <Grid container direction="column" spacing={2}>
+            <Grid item>
+              <h1>Register</h1>
+            </Grid>
+            <Grid item>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">Contact Type</FormLabel>
+                <RadioGroup
+                  aria-label="contact-type"
+                  name="contactType"
+                  value={contactType}
+                  onChange={handleContactTypeChange}
+                >
+                  <FormControlLabel value="email" control={<Radio />} label="Email" />
+                  <FormControlLabel value="phone" control={<Radio />} label="Phone Number" />
+                </RadioGroup>
+              </FormControl>
+            </Grid>
+            <Grid item>
+              {contactType === 'email' ? (
+                <TextField
+                  label="Email"
+                  type="email"
+                  value={contact}
+                  onChange={handleContactChange}
+                  required
+                />
+              ) : (
+                <TextField
+                  label="Phone Number"
+                  type="tel"
+                  value={contact}
+                  onChange={handleContactChange}
+                  required
+                />
+              )}
+            </Grid>
+            <Grid item>
+              <TextField
+                label="Password"
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+            </Grid>
+            <Grid item>
+              <Button type="submit" variant="contained" color="primary" fullWidth>
+                Register
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item style={{ position: 'absolute', top: 10, right: 10 }}>
+          <Button onClick={handleLoginRedirect} variant="outlined" color="primary">
+            Login
+          </Button>
+          {redirect && <Navigate to={"/login"}/>}
+        </Grid>
+      </Grid>
+    </form>
   );
-};
+}
 
 export default Register;
