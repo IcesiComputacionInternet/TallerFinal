364cc553a9a776901e21173e58eb88ddb1b9f5ed
DaniloErazo <erazodanilo1218@gmail.com>
2023-06-09 17:39:07 -0500

    Integration test happy path for item

--- e-shop-backend/src/main/java/com/example/eshopbackend/dto/ItemDTO.java
+++ e-shop-backend/src/main/java/com/example/eshopbackend/dto/ItemDTO.java
@@ -1,14 +1,18 @@
 package com.example.eshopbackend.dto;
 
 import com.example.eshopbackend.model.Category;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemDTO {
     private String itemId;
     @NotBlank
