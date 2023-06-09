2a0b0d28e9a595b3c1980fa35ea168c872a0294b
Juan Pablo Ramos Torres <61593415+Juan-dev123@users.noreply.github.com>
2023-06-09 13:07:28 -0500

    integration tests for item controller

--- backAutomoviles/src/test/java/co/icesi/automoviles/integration/ItemControllerTest.java
+++ backAutomoviles/src/test/java/co/icesi/automoviles/integration/ItemControllerTest.java
@@ -2,6 +2,7 @@ package co.icesi.automoviles.integration;
 
 import co.icesi.automoviles.TestConfigurationData;
 import co.icesi.automoviles.api.CategoryAPI;
+import co.icesi.automoviles.api.EShopUserAPI;
 import co.icesi.automoviles.api.ItemAPI;
 import co.icesi.automoviles.dto.*;
 import co.icesi.automoviles.model.Category;
@@ -19,10 +20,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.Optional;
+import java.util.UUID;
 
-import static co.icesi.automoviles.utils.DTOBuilder.defaultCategoryCreateDTO;
-import static co.icesi.automoviles.utils.DTOBuilder.defaultItemCreateDTO;
+import static co.icesi.automoviles.utils.DTOBuilder.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -158,7 +161,9 @@ public class ItemControllerTest {
                 .build();
         mockMvc.perform(MockMvcRequestBuilders.patch(ItemAPI.ROOT_PATH+"/e9c14553-3e76-4968-b78c-0d6fc8dfcdbb")
                 .header(HttpHeaders.AUTHORIZATION, "Bearer "+tokenDTO.getToken())
-                .content(objectMapper.writeValueAsString(originalItem)));
+                .content(objectMapper.writeValueAsString(originalItem))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
     }
 
     @Test
@@ -202,31 +207,95 @@ public class ItemControllerTest {
                 .build();
         mockMvc.perform(MockMvcRequestBuilders.patch(ItemAPI.ROOT_PATH+"/e9c14553-3e76-4968-b78c-0d6fc8dfcdbb")
                 .header(HttpHeaders.AUTHORIZATION, "Bearer "+tokenDTO.getToken())
-                .content(objectMapper.writeValueAsString(originalItem)));
+                .content(objectMapper.writeValueAsString(originalItem))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
     }
 
     @Test
     public void  testEndpointForGetItemByIdWithoutAnyCredentials() throws Exception {
-
-    }
+        var result = mockMvc.perform(MockMvcRequestBuilders.get(ItemAPI.ROOT_PATH+"/e9c14553-3e76-4968-b78c-0d6fc8dfcdbb")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        ItemShowDTO itemShowDTO = objectMapper.readValue(result.getResponse().getContentAsString(), ItemShowDTO.class);
+        assertEquals("e9c14553-3e76-4968-b78c-0d6fc8dfcdbb", itemShowDTO.getItemId().toString());
+        assertEquals("Description for car 1", itemShowDTO.getDescription());
+        assertEquals("Car 1", itemShowDTO.getName());
+        assertEquals(10000000, itemShowDTO.getPrice());
+        assertEquals("https://img.freepik.com/fotos-premium/fondos-coches-deportivos-azules_2227-2.jpg?w=500", itemShowDTO.getImageUrl());
+        assertEquals("246ccb5e-33e8-4d5a-9dc1-06bdc0ecf3ae", itemShowDTO.getCategory().getCategoryId().toString());
+        }
 
     @Test
     public void  testEndpointForGetAllItemsWithoutAnyCredentials() throws Exception {
+        var result = mockMvc.perform(MockMvcRequestBuilders.get(ItemAPI.ROOT_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        var itemShowDTO = objectMapper.readValue(result.getResponse().getContentAsString(), LinkedHashMap.class);
+        assertEquals(5, ((ArrayList) itemShowDTO.get("items")).size());
+        assertEquals(1, itemShowDTO.get("page"));
+        assertEquals(2, itemShowDTO.get("totalPages"));
+        assertEquals(8, itemShowDTO.get("count"));
+    }
 
+    private ItemShowDTO createItem() throws Exception {
+        ItemCreateDTO itemCreateDTO = defaultItemCreateDTO();
+        var result = mockMvc.perform(MockMvcRequestBuilders.post(ItemAPI.ROOT_PATH)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer "+tokenDTO.getToken())
+                        .content(objectMapper.writeValueAsString(itemCreateDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        ItemShowDTO itemShowDTO = objectMapper.readValue(result.getResponse().getContentAsString(), ItemShowDTO.class);
+        return itemShowDTO;
     }
 
     @Test
     public void  testEndpointForItemDeletionWithAdminCredentials() throws Exception {
-
+        loginAsAdmin();
+        ItemShowDTO itemShowDTO = createItem();
+        ItemCreateDTO itemCreateDTO = defaultItemCreateDTO();
+        var result = mockMvc.perform(MockMvcRequestBuilders.delete(ItemAPI.ROOT_PATH+"/"+itemShowDTO.getItemId())
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer "+tokenDTO.getToken())
+                        .content(objectMapper.writeValueAsString(itemCreateDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        assertTrue(result.getResponse().getContentAsString().isEmpty());
     }
 
     @Test
     public void  testEndpointForItemDeletionWithUserCredentials() throws Exception {
-
+        loginAsUser();
+        ItemCreateDTO itemCreateDTO = defaultItemCreateDTO();
+        var result = mockMvc.perform(MockMvcRequestBuilders.delete(ItemAPI.ROOT_PATH+"/e9c14553-3e76-4968-b78c-0d6fc8dfcdbb")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer "+tokenDTO.getToken())
+                        .content(objectMapper.writeValueAsString(itemCreateDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden())
+                .andReturn();
+        assertTrue(result.getResponse().getContentAsString().isEmpty());
     }
 
     @Test
     public void  testEndpointForItemDeletionWithShopCredentials() throws Exception {
-
+        loginAsShop();
+        ItemShowDTO itemShowDTO = createItem();
+        ItemCreateDTO itemCreateDTO = defaultItemCreateDTO();
+        var result = mockMvc.perform(MockMvcRequestBuilders.delete(ItemAPI.ROOT_PATH+"/"+itemShowDTO.getItemId())
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer "+tokenDTO.getToken())
+                        .content(objectMapper.writeValueAsString(itemCreateDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        assertTrue(result.getResponse().getContentAsString().isEmpty());
     }
 }
