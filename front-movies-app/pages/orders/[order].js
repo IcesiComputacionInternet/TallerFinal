a62cf3039a7d489f8926d2caef7d78f73610e3d6
Martin-Cifuentes <marcisego@gmail.com>
2023-06-08 23:42:55 -0500

    Showorders

--- front-movies-app/pages/orders/[order].js
+++ front-movies-app/pages/orders/[order].js
@@ -4,8 +4,7 @@ import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from '../../components/Navigation'
 
-
-export default function orders() {
+export default function orderDetails() {
 
     const router = useRouter();
 
@@ -24,9 +23,9 @@ export default function orders() {
           getCategories()
           getCurrentUser()
         }
-      }, [router.query.email]);
+    }, [router.query.email]);
 
-      async function getCurrentUser() {
+    async function getCurrentUser() {
         try {
           const {data} = await axios.get("http://localhost:8080/users/CurrentUser",
           {
@@ -45,28 +44,6 @@ export default function orders() {
     
         } catch (err) {
           console.error("Error fetching current user:", err);
-        }
-      }
-
-      async function getOrders(email) {
-        try {
-          const {data} = await axios.get("http://localhost:8080/orders/getUserOrders/" + email,
-          {
-              headers: {
-                  "Access-Control-Allow-Origin": "http://localhost:8080",
-                  "MediaType": "application/json",
-                  "Authorization": "Bearer " + localStorage.getItem('jwt')
-              }
-      
-          })
-    
-          let res = {data}
-          console.log(res)
-          setOrders(res)
-          setIsLoading(false)
-    
-        } catch (err) {
-          console.error("Error fetching movies:", err);
         }
       }
 
@@ -92,40 +69,30 @@ export default function orders() {
         }
       }
 
-    return(
-        <div>
-            {isLoadingCategories || isLoadingCurrent ? (
-                <h1>Loading...</h1>
-            ) : (
-                <Navigation user={current.data} categories={categories}/>
-            )}
+      
+      async function getOrders(email) {
+        try {
+          const {data} = await axios.get("http://localhost:8080/orders/getUserOrders/" + email,
+          {
+              headers: {
+                  "Access-Control-Allow-Origin": "http://localhost:8080",
+                  "MediaType": "application/json",
+                  "Authorization": "Bearer " + localStorage.getItem('jwt')
+              }
+      
+          })
+    
+          let res = {data}
+          console.log(res)
+          setOrders(res)
+          setIsLoading(false)
+    
+        } catch (err) {
+          console.error("Error fetching movies:", err);
+        }
+      }
 
-            <div className="text-center">
-                {isLoadingCurrent ? (
-                    <h1>Loading...</h1>
-                ) : (
-                    <h1>{current.data.firstName}'s Orders</h1>
-                )}
+      
 
-                {isLoading ? (
-                    <h1>Loading...</h1>
-                ):(
-                    <div>
-                        <ul className="list-group">
-                            {orders.data.map((order) => (
-                                <a href="#" class="list-group-item list-group-item-action">
-                                <div className="d-flex w-100 justify-content-between">
-                                  <h5 className="mb-1">{order.orderNumber}</h5>
-                                  <small className="text-muted">{order.status}</small>
-                                </div>
-                                <p className="mb-1">Total cost: {order.total}</p>
-                                <small>Click to see the content.</small>
-                              </a>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-            </div>
-        </div>
-    )
+    
 }
