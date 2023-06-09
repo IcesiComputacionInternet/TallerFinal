b99828880627025f4126013b44487e835037dbbc
BrayanOB2003 <brayanortizbastidas82@gmail.com>
2023-06-08 21:16:53 -0500

    Added new frontend strcuture

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div>
+      <h2>Registro</h2>
+      {/* Aquí puedes agregar el formulario de registro */}
+    </div>
+  );
+};
+
+export default Register;
